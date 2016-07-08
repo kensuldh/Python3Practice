11fa8639151dfc5626e1a1e68cd4bb9b998c94e0
kensuke sakai <spiknsk@gmail.com>
2016-07-08 11:39:53 +0900

    衝突を修正

--- StreamingAPI3.py
+++ StreamingAPI3.py
@@ -15,17 +15,13 @@ retry_max = 10
 retry_time = 10
 
 #ここにはTwitter Devで登録したアプリケーションのCONSUMER_KEY,SECRETを記入する。
-<<<<<<< HEAD
 CONSUMER_NAME = 'APITest_spiknsk'
-=======
-CONSUMER_NAME = 'YOUR APPLICATION NAME'
->>>>>>> eb136d6838aca83592f0394af8065dfd3742c991
 CONSUMER_KEY =  'PUhRhZcpxbcpd2eUWgjdvxb1N'
 CONSUMER_SECRET = 'qb3JPFWXCfEWSM8EVFHbrZHnaDCEOx84YptoXNhsCrTpNDeVES'
 
 #過去に認証したときのTokenを探す。
 TWITTER_CREDS = os.path.expanduser('.credentials')
-#if not os.path.exists(TWITTER_CREDS):
+if not os.path.exists(TWITTER_CREDS):
   #無ければ、OAuthで認証
   oauth_dance(CONSUMER_NAME, CONSUMER_KEY, CONSUMER_SECRET, TWITTER_CREDS)
 oauth_token, oauth_secret = read_token_file(TWITTER_CREDS)
@@ -35,45 +31,45 @@ oauth_token, oauth_secret = read_token_file(TWITTER_CREDS)
 stream = TwitterStream(auth=OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
 twitter = Twitter(auth=OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
 
-##in_reply_to_status_idから宛先となるツイートを参照する。
-#def show(_status):
-#  #print('IN:\n'+str(_status), file=sys.stderr)
-#  #リトライを設定
-#  for r in range(retry_max):
-#    #言語が日本語、in_reply_to_status_idが明示されており、禁止単語を含まないツイートの場合
-#    if 'lang' in _status and _status['lang'] == 'ja' and 'in_reply_to_status_id' in _status and not _status['in_reply_to_status_id'] is None and 'text' in _status and check(_status['text']):
-#      try:
-#        #ツイートを参照
-#        status=twitter.statuses.show(id=_status['in_reply_to_status_id'])
-#      except:
-#        #print('! Error\tRetry to read in_reply_tweet - '+str(r), file=sys.stderr)
-#        time.sleep(retry_time)
-#        continue
-#      #print('OUT:\n'+str(status), file=sys.stderr)
-#      #宛先のツイートが日本語、in_reply_to_status_idが明示されており、禁止単語を含まないツイートの場合
-#      if 'lang' in _status and _status['lang'] == 'ja' and 'text' in status and check(status['text']):
-#        #標準出力で応答の対を出力
-#        print(str(status['id'])+'\t'+trim(status['text'])+'\t'+str(_status['id'])+'\t'+trim(_status['text']))
-#        #さらに、宛先のツイートがin_reply_to_status_idを持つ場合は、さらに参照を行う。
-#        if 'in_reply_to_status_id' in status and not status['in_reply_to_status_id'] is None:
-#          show(status)
-#        break
-#      else:
-#        #print('! Info\tin_reply_tweet is not acceptable', file=sys.stderr)
-#        break
-#    else:
-#      break
+#in_reply_to_status_idから宛先となるツイートを参照する。
+def show(_status):
+ #print('IN:\n'+str(_status), file=sys.stderr)
+ #リトライを設定
+ for r in range(retry_max):
+   #言語が日本語、in_reply_to_status_idが明示されており、禁止単語を含まないツイートの場合
+   if 'lang' in _status and _status['lang'] == 'ja' and 'in_reply_to_status_id' in _status and not _status['in_reply_to_status_id'] is None and 'text' in _status and check(_status['text']):
+     try:
+       #ツイートを参照
+       status=twitter.statuses.show(id=_status['in_reply_to_status_id'])
+     except:
+       #print('! Error\tRetry to read in_reply_tweet - '+str(r), file=sys.stderr)
+       time.sleep(retry_time)
+       continue
+     #print('OUT:\n'+str(status), file=sys.stderr)
+     #宛先のツイートが日本語、in_reply_to_status_idが明示されており、禁止単語を含まないツイートの場合
+     if 'lang' in _status and _status['lang'] == 'ja' and 'text' in status and check(status['text']):
+       #標準出力で応答の対を出力
+       print(str(status['id'])+'\t'+trim(status['text'])+'\t'+str(_status['id'])+'\t'+trim(_status['text']))
+       #さらに、宛先のツイートがin_reply_to_status_idを持つ場合は、さらに参照を行う。
+       if 'in_reply_to_status_id' in status and not status['in_reply_to_status_id'] is None:
+         show(status)
+       break
+     else:
+       #print('! Info\tin_reply_tweet is not acceptable', file=sys.stderr)
+       break
+   else:
+     break
 
-##改行を適当に置き換える
-#def trim(text):
-#  return text.replace('\r','-br-').replace('\n','-br-')
+#改行を適当に置き換える
+def trim(text):
+ return text.replace('\r','-br-').replace('\n','-br-')
 
-##禁止文字のチェックを行う
-#def check(text):
-#  for char in check_chara:
-#    if char in text:
-#      return False
-#  return True
+#禁止文字のチェックを行う
+def check(text):
+ for char in check_chara:
+   if char in text:
+     return False
+ return True
 
 #Streaming APIからツイートを読み出す
 def main():
